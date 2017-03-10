1d6742c13b9f89c4aaaffd9ee305a7966193f2a6
BruceMacD <brucewmacdonald@gmail.com>
2017-03-09 22:01:09 -0400

    Optimize search
    Set search params and simplify logic

--- FreeFoodScraper-Dalhousie.py
+++ FreeFoodScraper-Dalhousie.py
@@ -1,19 +1,21 @@
 from bs4 import BeautifulSoup, SoupStrainer
 import requests
+import urllib2
 
+#common words that indicate free food
+filters = ['breakfast', 'lunch', 'meal', 'coffee', 'snacks', 'refreshments', 'food', 'dinner', 'pizza']
+# all public events at Dalhousie are posted here
 url = 'https://www.dal.ca/news/events.html'
 webpage = requests.get(url)
 root_soup = BeautifulSoup(webpage.text, 'html.parser')
 
+print("Upcoming events at Dalhousie with free food:")
 # for all event links check for keywords
 for link in root_soup.find_all('a', href=True):
-    if '/events/' in link['href']:
-        event = requests.get(link['href'])
-        event_soup = BeautifulSoup(event.text, 'html.parser')
-        result = event_soup.find_all('p')
-        for x in result:
-            if 'lunch' in x.text:
+    # any events have format events/20**, including the year removes rss feed link
+    if '/events/2' in link['href']:
+        html = urllib2.urlopen(link['href']).read()
+        for keyword in filters:
+            if keyword in html.lower() and 'free' in html.lower():
                 print(link['href'])
-        #for body in event_soup.find_all('p'):
-        #    if 'lunch' in body:
-        #        print event_soup
+                break
