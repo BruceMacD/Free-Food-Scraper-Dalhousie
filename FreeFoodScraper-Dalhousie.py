bf84ed477844588fa7af8b5f5d22247bc6c37581
BruceMacD <brucewmacdonald@gmail.com>
2017-03-12 23:27:25 -0300

    Print to Twitter
    Output scan results to Twitter

--- FreeFoodScraper-Dalhousie.py
+++ FreeFoodScraper-Dalhousie.py
@@ -1,21 +1,47 @@
 from bs4 import BeautifulSoup, SoupStrainer
 import requests
 import urllib2
+import tweepy
+from time import strftime, localtime
 
-#common words that indicate free food
-filters = ['breakfast', 'lunch', 'meal', 'coffee', 'snacks', 'refreshments', 'food', 'dinner', 'pizza']
 # all public events at Dalhousie are posted here
 url = 'https://www.dal.ca/news/events.html'
-webpage = requests.get(url)
-root_soup = BeautifulSoup(webpage.text, 'html.parser')
+# Twitter API setup
+consumer_key = 'XXX'
+consumer_secret = 'XXX'
+access_token = 'XXX'
+access_token_secret = 'XXX'
 
-print("Upcoming events at Dalhousie with free food:")
-# for all event links check for keywords
-for link in root_soup.find_all('a', href=True):
-    # any events have format events/20**, including the year removes rss feed link
-    if '/events/2' in link['href']:
-        html = urllib2.urlopen(link['href']).read()
-        for keyword in filters:
-            if keyword in html.lower() and 'free' in html.lower():
-                print(link['href'])
-                break
+
+def scan_events():
+    webpage = requests.get(url)
+    root_soup = BeautifulSoup(webpage.text, 'html.parser')
+    print("Starting Dalhousie Free Food Scan:")
+    # for all event links check for keywords
+    for link in root_soup.find_all('a', href=True):
+        # only want links for the next week
+        current_day = strftime("%d", localtime())
+        current_month = strftime("%Y/%m/", localtime())
+        # while date in url < current datetime
+        for day in range(int(current_day), int(current_day) + 7):
+            if '/events/'+current_month+current_day in link['href']:
+                html = urllib2.urlopen(link['href']).read()
+                for keyword in filters:
+                    if keyword in html.lower() and 'free' in html.lower():
+                        write_tweet(keyword, link)
+
+
+def write_tweet(keyword, link):
+    tweet = "%s: %s" % (keyword, link['href'])
+    print(tweet)
+    # api.update_status(status=tweet)
+
+
+# common words that indicate free food
+filters = ['breakfast', 'lunch', 'meal', 'coffee', 'snacks', 'refreshments', 'food', 'dinner', 'pizza']
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+scan_events()
